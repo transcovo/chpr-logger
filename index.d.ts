063b07c423719e12c93dc25bbaf4b66a028f79fa
Vincent Schoener <vincent.schoener@chauffeur-prive.com>
2018-09-13 14:21:53 +0200

    TECH - Add Typescript support

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for chpr-logger 2.7.0
+// Project: chpr-logger
+// Definitions by: Chauffeur Privé
+// TypeScript Version: 3.0.1
+
+/// <reference types="node" />
+
+import Logger = require('bunyan');
+
+declare const logger: Logger;
+export = logger;
